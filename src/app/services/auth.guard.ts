b49239203b657d011bf8db6b022990cdfbc7e2dc
Angel Luna <ang_10_aflt@yahoo.com>
2020-07-27 17:25:47 -0500

    S-7 Authentication OK

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -8,7 +8,7 @@ import { tap } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  
+
   constructor( private authService: AuthService, private router: Router ) {}
 
   canActivate(): Observable<boolean> {
